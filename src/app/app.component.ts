7d8b90d479ea7432f981a967377c556a54cf04d3
Claudia <claudiabdm@hotmail.es>
2020-04-20 18:20:26 +0200

    Add some simple styles and a timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,9 +16,13 @@ export class AppComponent implements OnInit {
 
   questions: Question[];
   questionNumber: number = 0;
-  correctAns = 0;
+  correctAns: number = 0;
+  result: string = '';
   start: boolean = false;
   done: boolean = false;
+  disabledBtn: boolean = true;
+  seconds: number = 0;
+  counter;
 
   form: FormGroup;
 
@@ -37,13 +41,15 @@ export class AppComponent implements OnInit {
       this.questions.forEach(question => {
         question['options'] = this.getOptions(question);
       });
-      this.form = this.toFormGroup(this.questions)
+      this.form = this.toFormGroup(this.questions);
+      this.disabledBtn = false;
     });
   }
 
   onStart() {
     this.start = true;
     this.done = false;
+    this.counter = setInterval(() => {this.seconds += 1},1000);
   };
 
   onNext() {
@@ -51,11 +57,13 @@ export class AppComponent implements OnInit {
   }
 
   onSubmit() {
+    clearInterval(this.counter);
     this.questions.forEach((question, idx) => {
       if (question.correct_answer === this.form.value[idx]) {
         this.correctAns += 1;
       }
     })
+    this.result = this.correctAns >= 5 ? 'You passed the quiz!' : "Sorry, you didn't pass the quiz :(";
     this.done = true;
   }
 
@@ -78,7 +86,5 @@ export class AppComponent implements OnInit {
     options.push(question.correct_answer);
     return options;
   }
-
-
 
 }
--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, AfterViewInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 @Component({
@@ -14,11 +14,22 @@ export class QuestionComponent implements OnInit {
 
   constructor() { }
 
+  onSelect(e) {
+    this.form.value[this.idx] = e.target.innerText;
+    const selected = document.getElementsByClassName('question__selected');
+    Array.from(selected).forEach(elem => elem.classList.remove('question__selected'));
+    e.target.parentNode.classList.add('question__selected');
+    e.target.parentNode.classList.add('question__selected');
+  }
 
   ngOnInit(): void {
     this.shuffleOptions(this.question.options);
   }
 
+  get defaultOption() {
+    const defaultOption = this.form.value[this.idx];
+    return defaultOption;
+  }
 
   private shuffleOptions(arr): [] {
     for (let i = arr.length - 1; i > 0; i--) {
